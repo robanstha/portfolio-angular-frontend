a4d93e11850b02d8b84bd3d46530efbfc185be75
Roban Shrestha <robanstha@gmail.com>
2025-11-09 12:15:48 -0600

    (bugfix) Add OnInit import

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from './shared/theme.service';
 import { Observable } from 'rxjs';
